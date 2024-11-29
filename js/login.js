6a3a3b044fe9aa8c779dd2ae13eff8054220eaf8
Ornela Lo Presti <ornelalopresti@MacBook-Pro-de-Ornela.local>
2024-11-29 11:44:03 -0300

    login con alert

--- js/login.js
+++ js/login.js
@@ -2,17 +2,15 @@ let formulario = document.querySelector("#registerForm");
 let emailInput = document.querySelector("#email");
 let passwordInput = document.querySelector("#password");
 
-let emailError = document.querySelector("#emailError");
-let passwordError = document.querySelector("#passwordError");
-
 formulario.addEventListener('submit', function(event){
     
     if (emailInput.value == ""){
         event.preventDefault();
-        emailError.style.display = "block";
+        alert("Por favor compelte el campo de email")
+
     } 
     if (passwordInput.value == ""){
         event.preventDefault();
-        passwordError.style.display = "block";
+        alert("Por favor compelte el campo de contraseña")
     }
 });
